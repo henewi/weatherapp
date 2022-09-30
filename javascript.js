03407b3dbd5763ead676c712af324a3a4692c376
henewi <hpdunn2002@gmail.com>
2022-09-30 19:13:12 +0100

    big ass commit

--- javascript.js
+++ javascript.js
@@ -11,16 +11,28 @@ window.addEventListener('load',() => {
             long=position.coords.longitude;
             lat=position.coords.lat;
             const base = 'https://api.openweathermap.org/data/2.5/weather?lat=$%7Blat%7D&lon=$%7Blong%7D&appid=$%7Bapi%7D&units=metric';
-
+// fetching data //
             fetch(base)
                 .then((response) => {
                     return response.json
                 })
                 .then((data) => {
+                    console.log(data);
                     const {temp}=data.main;
                     const {place}=data.name;
                     const {description, icon}=data.weather[0];
-                    const {sunrise. sunset}=data.sys;
+                    const {sunrise, sunset}=data.sys;
+                    const iconURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+                    const sunriseGMT = new Date(sunrise * 1000);
+                    const sunsetGMT = new Date(sunset * 1000);
+// interact with DOM 
+                    iconImg.src = iconUrl;     
+                    loc.textContent = `${place}`;
+                    desc.textContent = `${description}`;
+                    tempC.textContent = `${temp.toFixed(2)} °C`;
+                    tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+                    sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
+                    sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;               
                 });
         });
     }
